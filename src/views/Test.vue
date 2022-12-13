8c510155e2ec034b47f15673b16ef95fd4eb8da9
Jaime Aleman <jjaimealeman@gmail.com>
2022-12-12 19:59:00 -0700

    Add test.vue file.

---
+++ src/views/Test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="pt-20">
+        <div class="flex flex-col md:flex-row">
+            <div><a class="hover:text-blue-500" href="#">Item 1</a></div>
+            <div><a class="hover:text-blue-500" href="#">Item 2</a></div>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+
+</script>
+
+
+<style scoped>
+
+</style>
