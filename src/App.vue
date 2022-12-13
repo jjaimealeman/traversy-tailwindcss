64282674967f17b817bea5ed01a4ca2627a76a86
Jaime Aleman <jjaimealeman@gmail.com>
2022-12-12 20:21:44 -0700

    Feat: add transition to app.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
 	<main>
 		<Navigation />
-		<router-view />
+		<Transition>
+			<router-view />
+		</Transition>
 		<Footer />
 	</main>
 </template>
 
 <script setup>
-    import Navigation from './components/Navigation.vue';
-    import Footer from './components/Footer.vue';
+import Footer from './components/Footer.vue';
+import Navigation from './components/Navigation.vue';
 </script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 1s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+</style>
