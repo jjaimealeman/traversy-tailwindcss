ac1da6a3cb9ca23b449fed60c43d16679edf62db
Jaime Aleman <jjaimealeman@gmail.com>
2022-12-14 17:52:15 -0700

    Fix: app.vue transition fade effect.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 	<main>
 		<div class="z-[60] fixed bottom-4 left-0 bg-black text-white border-[1px] border-white p-1 text-xs">{{ windowWidth }}x{{ windowHeight }}</div>
 		<Navigation />
-		<Transition>
-			<router-view />
-		</Transition>
+		<router-view v-slot="{ Component }">
+			<transition name="fade" mode="out-in">
+				<component :is="Component" />
+			</transition>
+		</router-view>
 		<Footer />
 	</main>
 </template>
@@ -14,31 +16,31 @@ import Footer from './components/Footer.vue';
 import Navigation from './components/Navigation.vue';
 
 export default {
-    data() {
-            return {
-                windowWidth: window.innerWidth,
-                windowHeight: window.innerHeight
-            }
-        },
-        mounted() {
-            window.onresize = () => {
-                this.windowWidth = window.innerWidth,
-                this.windowHeight = window.innerHeight
-                // console.log(this.windowWidth)
-            }
-        }
+	data() {
+		return {
+			windowWidth: window.innerWidth,
+			windowHeight: window.innerHeight
+		}
+	},
+	mounted() {
+		window.onresize = () => {
+			this.windowWidth = window.innerWidth,
+				this.windowHeight = window.innerHeight
+			// console.log(this.windowWidth)
+		}
+	}
 }
 
 </script>
 
 <style>
-.v-enter-active,
-.v-leave-active {
-	transition: opacity 1s ease;
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.25s ease;
 }
 
-.v-enter-from,
-.v-leave-to {
+.fade-enter-from,
+.fade-leave-to {
 	opacity: 0;
 }
 </style>
