9fb5eea5575eac6265c9534fd7a786a0df808383
Jaime Aleman <jjaimealeman@gmail.com>
2022-12-16 22:01:54 -0700

    Fix app.vue moved windowW/H to bottom right.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
 	<main>
-		<div class="z-[60] fixed bottom-4 left-0 bg-black text-white border-[1px] border-white p-1 text-xs">{{ windowWidth }}x{{ windowHeight }}</div>
+		<div class="z-[60] fixed bottom-0 right-0 bg-black text-white border-[1px] border-white p-1 text-xs">
+			{{ windowWidth }}x{{ windowHeight }}
+		</div>
 		<Navigation />
 		<router-view />
 		<Footer />
