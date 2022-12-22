2fd55655b399991622862980eeec947ecaa13702
Jaime Aleman <jjaimealeman@gmail.com>
2022-12-22 15:10:58 -0700

    Fix app.vue removed windowHeight/windowWidth and put in footer.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
 	<main>
-		<div class="z-[60] fixed bottom-0 right-0 bg-black text-white border-[1px] border-white p-1 text-xs">
-			{{ windowWidth }}x{{ windowHeight }}
-		</div>
 		<Navigation />
 		<router-view />
 		<Footer />
@@ -12,23 +9,6 @@
 <script>
 import Footer from './components/Footer.vue';
 import Navigation from './components/Navigation.vue';
-
-export default {
-	data() {
-		return {
-			windowWidth: window.innerWidth,
-			windowHeight: window.innerHeight
-		}
-	},
-	mounted() {
-		window.onresize = () => {
-			this.windowWidth = window.innerWidth,
-				this.windowHeight = window.innerHeight
-			// console.log(this.windowWidth)
-		}
-	}
-}
-
 </script>
 
 <style>
