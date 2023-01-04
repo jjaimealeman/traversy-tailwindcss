fae7af20c9e91784195379b73191be6b1d4c0ae8
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-04 13:46:39 -0700

    After adding editorconfig, all spacing now equals 4 instead of 2.

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="container mx-auto">
-    <ul class="bg-slate-300 p-2 mb-4">
-      <li class="inline-block mr-2"> <nuxt-link to="/">Home</nuxt-link> </li>
-      <li class="inline-block"> About </li>
-    </ul>
-    <div class="flex items-center justify-center"></div>
-    <h1 class="text-2xl font-bold">This is about page.</h1>
-  </div>
+    <div class="container mx-auto">
+        <ul class="bg-slate-300 p-2 mb-4">
+            <li class="inline-block mr-2">
+                <nuxt-link to="/">Home</nuxt-link>
+            </li>
+            <li class="inline-block">About</li>
+        </ul>
+        <div class="flex items-center justify-center">
+            <h1 class="text-2xl font-bold">This is about page.</h1>
+        </div>
+    </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="container mx-auto">
-    <ul class="bg-slate-300 p-2 mb-4">
-      <li class="inline-block mr-2">Home</li>
-      <li class="inline-block">
-        <NuxtLink class="block" to="/about"> About </NuxtLink>
-      </li>
-    </ul>
-
-    <TheCard />
-
-  </div>
+    <div class="container mx-auto">
+        <ul class="bg-slate-300 p-2 mb-4">
+            <li class="inline-block mr-2">Home</li>
+            <li class="inline-block">
+                <NuxtLink class="block" to="/about"> About </NuxtLink>
+            </li>
+        </ul>
+        <EmailSubscribe />
+        <TheCard />
+        <TheCard />
+    </div>
 </template>
-
