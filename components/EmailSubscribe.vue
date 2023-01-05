8c7e5930cb7996d3dd636fe6475519383976031b
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-04 23:35:08 -0700

    Add emailscribe component with bg image.

--- components/EmailSubscribe.vue
+++ components/EmailSubscribe.vue
@@ -1,46 +1,32 @@
 <template>
-    <div
-        class="myzinc flex items-center justify-center py-4 my-4 rounded-2xl shadow-lg mx-4"
-    >
+    <section class="myzinc flex items-center justify-center py-4 my-4 rounded-2xl shadow-lg mx-4">
         <div class="p-2 mx-6 rounded-2xl">
             <div class="flex flex-col md:flex-row rounded-xl overflow-hidden">
-                <img
-                    class="- object-cover rounded-xl h-80 transform duration-200 - md:h-64 md:rounded-l-xl md:rounded-r-none - hover:scale-105 -"
-                    src="~/assets/image.jpg"
-                    alt=""
-                />
+                <img class="
+                    object-cover rounded-xl h-80 transform duration-200
+                    md:h-64 md:rounded-l-xl md:rounded-r-none
+                    hover:scale-105
+                    " src="~/assets/image.jpg" alt="">
                 <div class="p-6 md:p-12">
-                    <h2
-                        class="text-xl font-medium text-center text-white md:text-left"
-                    >
-                        Subscribe to our newsletter
+                    <h2 class="text-xl font-medium text-center text-white md:text-left">
+                        Subscribe to our newsletter.
                     </h2>
-                    <p
-                        class="max-w-xs my-4 text-xs leading-5 tracking-wide text-center text-white md:text-left"
-                    >
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Magni, esse?
+                    <p class="max-w-xs my-4 text-xs leading-5 tracking-wide text-center text-white md:text-left">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, adipisci.
                     </p>
-                    <div
-                        class="flex flex-col mt-5 space-y-4 md:space-x-3 md:flex-row md:space-y-0"
-                    >
-                        <input
-                            class="p-2 px-4 text-center md:text-left border text-white placeholder:text-xs placeholder:text-center placeholder:md:text-left focus:outline-none"
-                            type="text"
-                            placeholder="Enter your email address."
-                        />
-                        <button
-                            class="px-5 py-3 text-xs rounded-md text-zinc-800 bg-emerald-500/75 hover:bg-emerald-500 hover:text-white duration-500"
-                        >
-                            <Icon class="inline" icon="ic:outline-email"></Icon>
+                    <div class="flex flex-col mt-5 space-y-4 md:space-x-3 md:flex-row md:space-y-0">
+                        <input class="p-2 px-4 text-center md:text-left border text-white placeholder:text-xs placeholder:text-center placeholder:md:text-left focus:outline-none" type="text" placeholder="Enter your email address.">
+                        <button class="px-5 py-3 text-xs rounded-md text-zinc-800 bg-emerald-500/75 hover:bg-emerald-500 hover:text-white duration-500">
+                            <Icon class="inline" name="ic:outline-email" />
                             Subscribe
                         </button>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </template>
+
 
 <style scoped>
 .myzinc {
@@ -72,6 +58,3 @@
 }
 </style>
 
-<script>
-import { Icon } from '@iconify/vue'
-</script>
