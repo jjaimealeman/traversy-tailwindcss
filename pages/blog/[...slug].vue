eceacd0c07ba9d0c4d5c1adf4dfa71fbe87b9b39
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-05 19:32:57 -0700

    Remove unnecessary classes.

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20" >
-        <div class="max-w-screen-sm sm:text-center sm:mx-auto">
-            <h2 class="mb-4 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none" > {{ post.title }} </h2>
+    <div class="container mx-auto bg-rose-300 min-h-screen">
+        <div class="">
+            <h2 class="" > {{ post.title }} </h2>
             <img :src="img(imageFile, { width: 500, format: 'webp' })" :alt="post.image_alt" />
-            <p class="text-base text-gray-700 md:text-lg sm:px-4"> {{ post.content }} </p>
+            <p class=""> {{ post.content }} </p>
             <!-- {{ post.image }} -->
             <!-- <img :src="img(imageFile)" alt=""> -->
             <!-- <img :src="img(imageFile)" alt="original" /> -->
