1305737be318f311a336fc8a1031af488a8f3a30
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-07 12:16:38 -0700

    Add Noto Sans font and use NuxtLayout.

--- app.vue
+++ app.vue
@@ -1,14 +1,22 @@
 <template>
     <TheHeader />
-    <NuxtPage />
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
     <TheFooter />
 </template>
 
-<style lang="postcss" scoped>
-.badge {
-  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 cursor-pointer;
-  &:hover {
-    @apply bg-gray-300;
-  }
-}
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap');
 </style>
+
+<script setup>
+    useHead({
+      link: [
+            { rel: 'preconnect', href: 'https://fonts.gstatic.com'},
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;0,400;0,700;1,300&display=swap'}
+      ]
+    })
+</script>
+
