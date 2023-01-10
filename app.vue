bed10b61b525f3241a718d3d596f217141599800
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-10 12:05:21 -0700

    Remove google fonts 3rd party http request and replace with self-hosted fonts.

--- app.vue
+++ app.vue
@@ -1,26 +1,29 @@
 <template>
-    <NuxtLayout>
-        <NuxtPage />
-    </NuxtLayout>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
 <style>
+/*
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap');
+*/
 </style>
 
 <script setup>
 useHead({
-    link: [
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-        {
-            rel: 'stylesheet',
-            href: 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;0,400;0,700;1,300&display=swap',
-        },
-    ],
-    bodyAttrs: {
-        class: 'debug-screens bg-zinc-900/[0.98]',
-        // script: [ { children: 'console.log(\'Hello world\')' } ]
-    },
+  //  link: [
+  //    { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+  //    {
+  //      rel: 'stylesheet',
+  //      href: 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;0,400;0,700;1,300&display=swap',
+  //    },
+  //  ],
+  bodyAttrs: {
+    //        class: 'debug-screens bg-zinc-901/[0.98]',
+    class: 'debug-screens bg-white-900/[0.98]',
+    // script: [ { children: 'console.log(\'Hello world\')' } ]
+  },
 });
 </script>
 
