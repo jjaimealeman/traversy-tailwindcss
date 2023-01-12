9a6cc8d61156845997b8420b853fb18fba451de6
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-12 15:19:00 -0700

    Add htmlAttrs to head in app.vue.

--- app.vue
+++ app.vue
@@ -19,22 +19,25 @@ useHead({
   //      href: 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;0,400;0,700;1,300&display=swap',
   //    },
   //  ],
-      link: [
-        { 
-          rel: 'preload', 
-          as: 'font', 
-          type: 'font/woff2', 
-          crossorigin: 'anonymous', 
-          href: '/fonts/noto-sans-v27-latin-regular.woff2' 
-        },
-        { 
-          rel: 'preload', 
-          as: 'font', 
-          type: 'font/woff2', 
-          crossorigin: 'anonymous', 
-          href: '/fonts/plus-jakarta-sans-v3-latin-700.woff2' 
-        }
-      ],
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'preload',
+      as: 'font',
+      type: 'font/woff2',
+      crossorigin: 'anonymous',
+      href: '/fonts/noto-sans-v27-latin-regular.woff2'
+    },
+    {
+      rel: 'preload',
+      as: 'font',
+      type: 'font/woff2',
+      crossorigin: 'anonymous',
+      href: '/fonts/plus-jakarta-sans-v3-latin-700.woff2'
+    }
+  ],
   bodyAttrs: {
     //        class: 'debug-screens bg-zinc-901/[0.98]',
     class: 'debug-screens bg-white-900/[0.98]',
