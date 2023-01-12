f68dc3fdc47e2ad8311e4c1f51cff7d7388ef1a0
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-12 13:56:42 -0700

    Remove toggle/EmailSubscribe from home.

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,16 @@
 <template>
-    <div class="container mx-auto">
-
-        <div class="border-2 text-center p-4">
-            <button class="bg-gray-200 rounded-lg p-2 " @click="awesome = !awesome">Toggle <Icon class="inline" name="ic:outline-email" /></button>
-            <h2 class="text-xl font-medium text-white" v-if="awesome">Subscribe to our newsletter.</h2>
-            <h2 class="text-white" v-else><EmailSubscribe /></h2>
-        </div>
-
-        <TheCard />
-    </div>
+  <div class="container mx-auto">
+    <EmailSubscribe class="zinc" />
+    <TheCard />
+  </div>
 </template>
 
 
 <script setup lang="ts">
-import { ref} from 'vue'
-const awesome = ref(true)
-
 useHead({
   title: 'Home',
   meta: [
     { name: 'description', content: 'My amazing site.' }
   ],
 });
-
-
-
 </script>
-
