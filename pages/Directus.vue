a1757c4e375b79ae148a4bfaa34b7ef5de05f0a4
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-12 15:18:32 -0700

    Fix image width for better Lighthouse score.

--- pages/Directus.vue
+++ pages/Directus.vue
@@ -6,7 +6,7 @@
           shadow-lg hover:shadow-2xl hover:scale-[1.02] hover:-translate-y-1 transition-all duration-200"
         v-for="post in posts">
         <NuxtLink :to="`/blog/${post.slug}`">
-          <img class="w-full rounded-t-md" :src="image + post.image" :alt="post.image_alt" />
+          <img class="w-full rounded-t-md" :src="image + post.image + imagekey" width="400" :alt="post.image_alt" />
           <p class="px-4 py-2 text-green-700 text-base"> {{
             new Date(post.date_created).toLocaleDateString('en-US', dateOptions)
           }}
@@ -29,6 +29,7 @@ const posts = await getItems({
   },
 });
 const image = 'https://y4aazew4.directus.app/assets/'
+const imagekey = '?fit=cover&width=400&quality=80'
 const dateOptions = {
   weekday: 'long',
   year: 'numeric',
