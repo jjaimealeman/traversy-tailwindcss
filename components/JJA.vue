5439b3034961498967396a0b1fafcb33b9263cef
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-13 22:09:19 -0700

    Add JJA component with my SVG logo.

---
+++ components/JJA.vue
@@ -0,0 +1,91 @@
+<template>
+  <svg width="39" height="33" viewBox="0 0 39 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M26.6631 6.53027L35.8059 24.816H17.5202L26.6631 6.53027Z" fill="url(#paint0_linear_402_30)" />
+    <g filter="url(#filter0_d_402_30)">
+      <path d="M35.8059 24.816L26.663 6.53027L26.3721 7.11209L35.2241 24.816H35.8059Z"
+        fill="url(#paint1_linear_402_30)" />
+    </g>
+    <path
+      d="M12.9488 6.53027H22.0916V15.6731C22.0916 20.7225 17.9982 24.8159 12.9488 24.8159C7.89932 24.8159 3.80594 20.7225 3.80591 15.6731H12.9488L12.9488 6.53027Z"
+      fill="url(#paint2_linear_402_30)" />
+    <g filter="url(#filter1_d_402_30)">
+      <path
+        d="M22.0916 6.53027H12.9487L13.3624 6.94574H21.684V16.0886C21.684 18.2812 20.9297 20.2936 19.6722 21.8688C21.1742 20.2397 22.0916 18.0636 22.0916 15.6731V6.53027Z"
+        fill="url(#paint3_linear_402_30)" />
+    </g>
+    <g filter="url(#filter2_d_402_30)">
+      <path d="M3.81521 16.0888L12.949 16.0888V15.6733H3.80591C3.80591 15.8126 3.80903 15.9511 3.81521 16.0888Z"
+        fill="url(#paint4_linear_402_30)" />
+    </g>
+    <defs>
+      <filter id="filter0_d_402_30" x="23.3721" y="3.53027" width="15.4338" height="24.2856"
+        filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha" />
+        <feOffset />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_402_30" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_402_30" result="shape" />
+      </filter>
+      <filter id="filter1_d_402_30" x="9.94873" y="3.53027" width="15.1429" height="21.3386"
+        filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha" />
+        <feOffset />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_402_30" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_402_30" result="shape" />
+      </filter>
+      <filter id="filter2_d_402_30" x="0.805908" y="12.6733" width="15.1431" height="6.41553"
+        filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha" />
+        <feOffset />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_402_30" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_402_30" result="shape" />
+      </filter>
+      <linearGradient id="paint0_linear_402_30" x1="26.6631" y1="6.53027" x2="17.5618" y2="24.7744"
+        gradientUnits="userSpaceOnUse">
+        <stop stop-color="#F9F871" />
+        <stop offset="0.986364" stop-color="#E19E20" />
+      </linearGradient>
+      <linearGradient id="paint1_linear_402_30" x1="26.663" y1="6.53027" x2="35.8059" y2="25.0253"
+        gradientUnits="userSpaceOnUse">
+        <stop stop-color="white" />
+        <stop offset="0.986364" stop-color="#F1DB57" />
+      </linearGradient>
+      <linearGradient id="paint2_linear_402_30" x1="12.9488" y1="6.53027" x2="12.9488" y2="24.8159"
+        gradientUnits="userSpaceOnUse">
+        <stop stop-color="#1BBB98" />
+        <stop offset="1" stop-color="#007CA0" />
+      </linearGradient>
+      <linearGradient id="paint3_linear_402_30" x1="13.2953" y1="16.7617" x2="22.0916" y2="6.53027"
+        gradientUnits="userSpaceOnUse">
+        <stop stop-color="#1AB998" />
+        <stop offset="0.296875" stop-color="#0C989C" />
+        <stop offset="1" stop-color="white" />
+      </linearGradient>
+      <linearGradient id="paint4_linear_402_30" x1="12.949" y1="15.8811" x2="3.80591" y2="15.8811"
+        gradientUnits="userSpaceOnUse">
+        <stop stop-color="white" />
+        <stop offset="1" stop-color="#0C989C" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'JJA'
+}
+</script>
