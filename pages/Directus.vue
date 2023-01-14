3f5d6a39b270219c95a9a4387a3627564caecb3d
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-14 00:19:26 -0700

    Add font sizes, colors, grid formatting and line-clamp to Directus.

--- pages/Directus.vue
+++ pages/Directus.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="container mx-auto">
-    <div class="grid md:grid-cols-2 lg:grid-cols-3 p-8 gap-10">
+    <div class="grid md:grid-cols-2 lg:grid-cols-3 m-4 md:m-10 gap-4 md:gap-10">
       <div class="
-          flex flex-col w-auto m-4 bg-white rounded-md overflow:hidden 
+          flex flex-col w-auto bg-white rounded-md overflow:hidden 
           shadow-lg hover:shadow-2xl hover:scale-[1.02] hover:-translate-y-1 transition-all duration-200"
         v-for="post in posts">
         <NuxtLink :to="`/blog/${post.slug}`">
           <img class="w-full rounded-t-md" :src="image + post.image + imagekey" width="400" :alt="post.image_alt" />
-          <p class="block md:hidden px-4 py-2 text-green-700 text-base"> {{
+          <p class="block md:hidden px-4 py-2 bg-zinc-200 text-zinc-600 text-xs"> {{
             new Date(post.date_created).toLocaleDateString('en-US',
-              dateOptions1)
+              dateOptionShort)
           }} </p>
-          <p class="hidden md:block px-4 py-2 text-green-700 text-base"> {{
-            new Date(post.date_created).toLocaleDateString('en-US',
-              dateOptions2)
-          }} </p>
+          <p class="hidden md:block px-4 py-2 bg-zinc-200 text-zinc-600 text-xs bg-gradient-to-r from-white to-sky-200">
+            {{
+              new Date(post.date_created).toLocaleDateString('en-US',
+                dateOptionLong)
+            }} </p>
         </NuxtLink>
-        <p class="px-4 pb-2 font-bold text-base">{{ post.title }}</p>
+        <p class="text-xl md:text-2xl text-zinc-700 px-4 py-2 font-bold">{{ post.title }}</p>
         <!--p class="px-4 pb-6" v-html="post.content2.slice(0, 200)"></p-->
-        <p class="px-4 pb-6 h-24 overflow-y-hidden overflow-clip leading-6" v-html="post.content2"></p>
+        <p class="text-lg md:text-xl text-zinc-700 mx-4 mb-4 line-clamp-4 md:line-clamp-6" v-html="post.content2"></p>
       </div>
     </div>
   </div>
@@ -35,13 +36,13 @@ const posts = await getItems({
 });
 const image = 'https://y4aazew4.directus.app/assets/'
 const imagekey = '?fit=cover&width=400&quality=80'
-const dateOptions1 = {
+const dateOptionShort = {
   weekday: 'short',
   year: 'numeric',
   month: 'short',
   day: 'numeric',
 }
-const dateOptions2 = {
+const dateOptionLong = {
   weekday: 'long',
   year: 'numeric',
   month: 'long',
