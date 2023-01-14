af42de54636679980e45ba583f0f71c19538cb03
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-13 22:07:40 -0700

    Made footer into flex with row/col, added hover to icons in footer.

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,15 +1,26 @@
 <template>
-  <footer class="bottom-0 w-full mt-8">
+  <footer class="bottom-0 w-full mt-8 p-4 bg-zinc-900/[0.95] text-zinc-400 border-t-2 border-t-blue-400 text-center">
     <!-- <p>Copyright &copy; {{ getDate }} | jjaimealeman </p> -->
-    <p>
-      Copyright &copy; {{ new Date().getFullYear() }} | jjaimealeman
-      <Icon name="logos:nuxt-icon" size="28" />
-      <Icon name="simple-icons:directus" size="28" />
-      <Icon name="logos:tailwindcss-icon" size="28" />
-      <Icon name="logos:netlify" size="28" />
-      <Icon name="logos:vercel-icon" size="28" />
+    <div class="flex flex-col md:flex-row md:px-12 justify-between italic">
+      <p class="text-xs">
+        by
+        <JJA class="inline w-8 h-8" />
+        with
+        <Icon name="lucide:coffee" size="16" />
+        &amp;
+        <Icon name="lucide:heart" size="16" />
+        <!-- {{ new Date().getFullYear() }} -->
+      </p>
+      <p class="tech">
+        <Icon class="hover:scale-110" name="logos:nuxt-icon" size="28" title="Nuxt3" />
+        <Icon class="hover:scale-110" name="simple-icons:directus" size="28" alt="Directus" />
+        <Icon class="hover:scale-110" name="simple-icons:astro" size="28" />
+        <Icon class="hover:scale-110" name="logos:tailwindcss-icon" size="28" />
+        <Icon class="hover:scale-110" name="logos:netlify" size="28" />
+        <Icon class="hover:scale-110" name="radix-icons:vercel-logo" size="28" />
+      </p>
 
-    </p>
+    </div>
     <div class="z-[60] fixed px-2 bottom-0 right-0 bg-black text-white border-[1px] border-white p-1 text-xs">
       {{ width }} x {{ height }}
     </div>
@@ -25,12 +36,8 @@ const getDate = useDateFormat(useNow(), 'YYYY')
 
 
 <style scoped>
-footer {
-  @apply bg-zinc-900/[0.95] text-zinc-400 w-full border-t-2 border-t-blue-400 p-2 text-center;
-}
-
-svg {
-  margin: 0 0.25rem;
+.tech svg {
+  @apply mx-2 mt-2
 }
 </style>
 
