6d348642677a54a0ef09fb1c0bd1eb9a89f47ee5
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-14 00:35:09 -0700

    Ready to start Flyo project.

---
+++ pages/Fylo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Fylo</h1>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
+
+
+<script setup>
+definePageMeta({
+  layout: 'none',
+});
+useHead({
+  title: 'Fylo Project',
+  meta: [{ name: 'description', content: 'My simple Directus Blog.' }],
+});
+
+</script>
