abd918f5ea7851416c1030550d583e0fd2f7bc4f
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-17 23:49:29 -0700

    Fix TheCard, random picsum, adjust layout for better mobile readability.

--- components/TheCard.vue
+++ components/TheCard.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="grid grid-cols-2 bg-zinc-100 text-gray-700 place-items-center py-8">
 
-    <div class="flex w-[400px]">
-      <!-- <div class="max-w-sm rounded-md overflow-hidden shadow-lg m-3"> -->
-      <div class="flex flex-col w-auto rounded-md overflow-hidden shadow-lg m-3">
-        <img class="w-full" src="https://i.imgur.com/rNspDMw.png" alt="Sunset in the mountains" />
+    <div class="flex flex-col">
+      <div class="max-w-sm rounded-md overflow-hidden shadow-lg m-3 bg-white text-gray-800 ring-2 ring-gray-200 dark:ring-0">
+      <!--div class="flex flex-col w-auto rounded-md overflow-hidden shadow-lg m-3"-->
+        <img class="w-full" src="https://picsum.photos/400/250" alt="Random picsum image" />
         <div class="px-6 py-4 pb-2">
-          <div class="font-bold text-xl mb-2">A Cool Autumn Day</div>
+          <div class="font-bold text-xl mb-2">Lorem ipsum dolor sit</div>
           <p class="text-gray-700 text-base">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Omnis eius reprehenderit aliquid esse et repellendus
@@ -14,36 +13,13 @@
           </p>
         </div>
         <div class="px-6 py-4">
-          <span class="badge mr-2 mb-2">#Trees</span>
-          <span class="badge mr-2 mb-2">#Leaves</span>
-          <span class="badge">#Fall</span>
+          <span class="badge mr-2 mb-2">#Alpha</span>
+          <span class="badge mr-2 mb-2">#Bravo</span>
+          <span class="badge">#Charlie</span>
         </div>
       </div>
     </div>
 
-    <div class="flex w-[400px]">
-      <!-- <div class="max-w-sm rounded-md overflow-hidden shadow-lg m-3"> -->
-      <div
-        class="flex flex-col w-auto rounded-md overflow-hidden shadow-lg hover:shadow-2xl m-3 transition-shadow duration-200">
-        <img class="w-full" src="https://i.imgur.com/rNspDMw.png" alt="Sunset in the mountains" />
-        <div class="px-6 py-4 pb-2">
-          <div class="font-bold text-xl mb-2">A Cool Autumn Day</div>
-          <p class="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Omnis eius reprehenderit aliquid esse et repellendus
-            exercitationem nostrum deserunt, harum dignissimos.
-          </p>
-        </div>
-        <div class="px-6 py-4">
-          <span class="badge mr-2 mb-2">#Trees</span>
-          <span class="badge mr-2 mb-2">#Leaves</span>
-          <span class="badge">#Fall</span>
-        </div>
-      </div>
-    </div>
-
-
-  </div>
 </template>
 
 <style lang="postcss" scoped>
@@ -51,7 +27,7 @@
   @apply inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 cursor-pointer;
 
   &:hover {
-    @apply bg-orange-400;
+    @apply bg-gray-400;
   }
 }
 </style>
