88c4d7882473ff7c524ec57011b7eede920992ff
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-18 16:29:59 -0700

    Remove all unnecessary configs from tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,39 +15,14 @@ module.exports = {
   ],
   theme: {
     // Screen setup for ALL Traversy projects.
-    screens: {
-      sm: '480px',   // 640px
-      md: '768px',   // 768px
-      lg: '976px',   // 1024px
-      xl: '1440px'   // 1280px
-    },
+    // screens: {
+      // sm: '480px',   // 640px
+      // md: '768px',   // 768px
+      // lg: '976px',   // 1024px
+      // xl: '1440px'   // 1280px
+    // },
     extend: {
       colors: {
-        // LoopStudios
-        strongCyan: 'hsl(171, 66%, 44%)',
-        lightBlue: 'hsl(233, 100%, 69%)',
-        darkGrayishBlue: 'hsl(210, 10%, 33%)',
-        grayishBlue1: 'hsl(201, 11%, 66%)',
-        // Shortly
-        cyan: 'hsl(180, 66%, 49%)',
-        cyanLight: 'hsl(180, 66%, 69%)',
-        darkViolet: 'hsl(257, 27%, 26%)',
-        red: 'hsl(0, 87%, 67%)',
-        grayishViolet: 'hsl(257, 7%, 63%)',
-        veryDarkBlue: 'hsl(255, 11%, 22%)',
-        veryDarkViolet: 'hsl(260, 8%, 14%) ',
-        // Fylo
-        darkBlue: 'hsl(217, 28%, 15%)',
-        darkBlue1: 'hsl(218, 28%, 13%)',
-        darkBlue2: 'hsl(216, 53%, 9%)',
-        darkBlue3: 'hsl(219, 30%, 18%)',
-        accentCyan: 'hsl(176, 68%, 64%)',
-        accentBlue: 'hsl(198, 60%, 50%)',
-        lightRed: 'hsl(1, 100% 63%)',
-        // Bookmark
-        softBlue: 'hsl(231, 69%, 60%)',
-        softRed: 'hsl(0, 94%, 66%)',
-        grayishBlue: 'hsl(229, 8%, 60%)',
         veryDarkBlue: 'hsl(229, 31%, 21%)',
       },
       fontFamily: {
