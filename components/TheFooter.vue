5aee831d2fe8c956b9e7b700ed05d30f8fde7ddd
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-18 12:15:29 -0700

    Remove WindowSize script and add title to SVG icons in TheFooter.

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -15,28 +15,17 @@
           <!-- {{ new Date().getFullYear() }} -->
         </p>
         <p class="tech">
-          <Icon class="hover:scale-110" name="logos:nuxt-icon" size="28" title="Nuxt3" />
+          <Icon class="hover:scale-110" name="logos:nuxt-icon" size="28" alt="Nuxt3" />
           <Icon class="hover:scale-110" name="simple-icons:directus" size="28" alt="Directus" />
-          <Icon class="hover:scale-110" name="simple-icons:astro" size="28" />
-          <Icon class="hover:scale-110" name="logos:tailwindcss-icon" size="28" />
-          <Icon class="hover:scale-110" name="logos:netlify" size="28" />
-          <Icon class="hover:scale-110" name="radix-icons:vercel-logo" size="28" />
+          <Icon class="hover:scale-110" name="simple-icons:astro" size="28" alt="Astro" />
+          <Icon class="hover:scale-110" name="logos:tailwindcss-icon" size="28" alt="TailwindCSS" />
+          <Icon class="hover:scale-110" name="logos:netlify" size="28" alt="Netlify" />
+          <Icon class="hover:scale-110" name="radix-icons:vercel-logo" size="28" alt="Vercel" />
         </p>
-      </div>
-      <div class="z-[60] fixed px-2 bottom-0 right-0 bg-black text-white border-[1px] border-white p-1 text-xs">
-        {{ width }} x {{ height }}
       </div>
     </div>
   </footer>
 </template>
-
-
-<script setup>
-// import { useDateFormat, useNow, useWindowSize } from '@vueuse/core';
-const { width, height } = useWindowSize();
-const getDate = useDateFormat(useNow(), 'YYYY')
-</script>
-
 
 <style scoped>
 .tech svg {
@@ -44,4 +33,5 @@ const getDate = useDateFormat(useNow(), 'YYYY')
 }
 </style>
 <!-- <p>Copyright &copy; {{ getDate }} | jjaimealeman </p> -->
+<!-- const getDate = useDateFormat(useNow(), 'YYYY') -->
 
