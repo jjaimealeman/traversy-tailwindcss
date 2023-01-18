4d93af355d603210b203e9135be33901bb497c57
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-18 12:41:26 -0700

    Add aria-label for darkmodetoggle in footer.

--- components/DarkMode.vue
+++ components/DarkMode.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="fixed bottom-16 right-0">
-    <button class="m-4 p-4 rounded-full bg-gray-200 ring-2 ring-blue-400 dark:bg-gray-800 text-gray-800 dark:text-gray-200" @click="toggleDark()">
+    <button role="button" aria-label="toggle dark mode" class="m-4 p-4 rounded-full bg-gray-200 ring-2 ring-blue-400 dark:bg-gray-800 text-gray-800 dark:text-gray-200" @click="toggleDark()">
       <!--span> {{ isDark? 'Dark': 'Light' }} </span-->
       <!-- LIGHT -->
       <div class="inline dark:hidden">
