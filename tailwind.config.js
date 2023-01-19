b454f2b1bf84e3b3f4b4d99bbe75fb6b1df94e7f
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-18 20:32:53 -0700

    Readd the colors from previous projects.

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,12 +23,39 @@ module.exports = {
     // },
     extend: {
       colors: {
+        // LoopStudios
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue1: 'hsl(201, 11%, 66%)',
+        // Shortly
+        cyan: 'hsl(180, 66%, 49%)',
+        cyanLight: 'hsl(180, 66%, 69%)',
+        darkViolet: 'hsl(257, 27%, 26%)',
+        grayishViolet: 'hsl(257, 7%, 63%)',
+        veryDarkBlue: 'hsl(255, 11%, 22%)',
+        veryDarkViolet: 'hsl(260, 8%, 14%) ',
+        // Fylo
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: 'hsl(219, 30%, 18%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(1, 100% 63%)',
+        // Bookmark
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
         veryDarkBlue: 'hsl(229, 31%, 21%)',
       },
       fontFamily: {
         // sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         sans: ['Noto Sans', ...defaultTheme.fontFamily.sans],
         bold: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+        // NEW FONTS: Architect Daughter and Be Vietnam
+        arch: ['Architects Daughter', 'cursive'],
+        viet: ['Be Vietnam Pro', 'sans-serif'],
       },
       letterSpacing: {
         widest: '0.3em'
