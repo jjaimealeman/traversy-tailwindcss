862496cef48a6310d8d8a85711a48c5503cc95fb
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-19 13:02:09 -0700

    Add Services page.

---
+++ pages/Services.vue
@@ -0,0 +1,196 @@
+<template>
+  <div class="container mx-auto pb-8 lg:pb-16 xl:pb-20">
+    <div
+      class="flex flex-col border-t-4 border-t-yellow-400 border-b-4 border-b-yellow-500 p-4 md:p-8 bg-gradient-to-b from-yellow-200 to-yellow-100/90">
+      <div class="w-full">
+        <h2 class="text-2xl md:text-3xl lg:text-4xl"> Here’s the dealio! </h2>
+      </div>
+      <div class="flex flex-col md:flex-row md:space-x-4 space-y-0">
+        <div class="w-full md:w-1/2 p-4 pb-0">
+          <h3 class="text-base md:text-lg lg:text-xl underline pl-4 pt-4"> All Dealios include: </h3>
+          <ul class="list-none list-outside pl-4">
+            <li class="check pt-4 text-lg md:text-xl">
+              Fully custom
+            </li>
+            <li class="text-lg md:text-xl">
+              100% hand-coded HTML, CSS, &amp; JavaScript. 
+            </li>
+            <li class="text-lg md:text-xl">
+              Responsive themes and layouts. 
+            </li>
+            <li class="text-lg md:text-xl pb-4">
+              Beautiful colors and legible fonts.
+            </li>
+            <li class="check text-lg md:text-xl">
+              Domain name registration and management.
+            </li>
+            <li class="text-lg md:text-xl pb-4">
+              With included Privacy Protection.
+            </li>
+            <li class="check text-lg md:text-xl">
+              Reliable blazing fast hosting of all assets: files, photos &amp; videos.
+            </li>
+            <li class="text-lg md:text-xl pb-4">
+              Includes free SSL Certificate.
+            </li>
+            <li class="check text-lg md:text-xl">
+              Contact Form
+            </li>
+            <li class="text-lg md:text-xl pb-4">
+              Receive emails from interested visitors about your products and services.
+            </li>
+            <li class="check text-lg md:text-xl pb-4">
+              Daily &amp; Weekly backups.
+            </li>
+            <li class="check text-lg md:text-xl pb-4">
+              All images resized &amp; compressed for mobile devices, and all code optimized for the fastest download
+              speeds.
+            </li>
+            <li class="check text-lg md:text-xl pb-4">
+              Instant roll-back to previous website versions.
+            </li>
+          </ul>
+        </div>
+        <div class="w-full md:w-1/2 p-4 pt-0 md:pt-4">
+          <h3 class="text-base md:text-lg lg:text-xl underline pl-4 pt-4"> Additional Services: </h3>
+          <ul class="list-none list-outside pl-4">
+            <li class="check pt-4 text-lg md:text-xl">
+              SEO 
+            </li>
+            <li class="text-lg md:text-xl pb-4">
+              I’ll work very hard to get your business on the first page of Google.
+              Regular updates and optimizations for search engine discovery.
+              Individual page updates to maintain optimal results.
+            </li>
+            <li class="check text-lg md:text-xl">
+              Analytics
+            </li>
+            <li class="text-lg md:text-xl pb-4">
+              See where your visitors are coming from, which pages are most/least popular,
+              and what other websites are sending you traffic.
+            </li>
+            <li class="check text-lg md:text-xl pb-4">
+              Custom logo design &amp; branding
+            </li>
+            <li class="check text-lg md:text-xl pb-4">
+              A secondary temporary unique domain to view and demo Work-In-Progress.
+            </li>
+            <li class="check text-lg md:text-xl pb-4">
+              English &amp; Spanish language translation.
+            </li>
+            <li class="check text-lg md:text-xl pb-4">
+              Your own custom Control Panel to add/update/delete your own content.
+            </li>
+            <li class="check text-lg md:text-xl pb-4">
+              Your own email address: info@example.com, sales@example.com
+            </li>
+            <li class="check text-lg md:text-xl pb-4">
+              Multi-theme switcher. Instantly change your website color scheme with a click!
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="flex flex-row md:flex-col mt-8 md:mt-16">
+      <div class="flex flex-col md:flex-row md:space-x-4">
+        <div class="w-full md:w-1/3 bg-gradient-to-b from-red-200 to-red-100/90 border-t-4 border-t-red-500 p-4">
+          <h2 class="text-2xl md:text-3xl lg:text-4xl"> What I don't do! </h2>
+          <p class="py-4">
+            No Wix.
+          </p>
+          <p class="pb-4">
+            No WordPress.
+          </p>
+          <p class="pb-4">
+            No SquareSpace.
+          </p>
+          <p class="pb-4">
+            I don’t use premade templates.
+          </p>
+          <p class="">
+            No content writing.
+          </p>
+          <p class="pb-4 italic">
+            You provide the content and I will build your website.
+          </p>
+        </div>
+        <div
+          class="w-full md:w-1/3 bg-gradient-to-b from-green-200 to-green-100/90 border-t-4 border-t-green-500 p-4 mt-8 md:mt-0">
+          <h2 class="text-2xl md:text-3xl lg:text-4xl"> What I do do! </h2>
+          <p class="py-4">
+            Every line of code is 100% lovingly Hand Crafted
+            <Icon class="animate-pulse" name="twemoji:revolving-hearts" />.
+          </p>
+          <p class="">
+            I generate 100% fully custom templates and layouts.
+          </p>
+          <p class="pb-4">
+            I guarantee NO OTHER WEBSITE will look like yours!
+          </p>
+          <p class="">
+            It takes time, but the results are impressive!
+          </p>
+          <p class="pb-4">
+            Responsive, Performant, and Accessible.
+          </p>
+          <p class="pb-4">
+            I test across all popular smartphones, laptops, desktops, browsers, and Operating Systems to ensure proper
+            functionality and performance.
+          </p>
+        </div>
+        <div
+          class="w-full md:w-1/3 bg-gradient-to-b from-green-200 to-green-100/90 border-t-4 border-t-green-500 p-4 mt-8 md:mt-0">
+          <p class="pb-4 pt-0 md:pt-14">
+            I will sit with you and determine the strong keywords needed to generate Google results and consistent
+            traffic.
+          </p>
+          <p class="pb-4">
+            Together we will choose several fonts that will be used throughout the site: navigation, headers, body, etc.
+          </p>
+          <p class="pb-4">
+            You and I will explore various color palettes that result in a visually appealing presence.
+          </p>
+          <p class="pb-4">
+            Daily communication (during normal business hours).
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+h1,
+h2,
+p {
+  @apply font-viet text-zinc-700;
+}
+
+h1 {
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.arch {
+  @apply font-arch;
+}
+
+.check {
+  list-style-image: url("~/assets/checkbox2.svg");
+}
+</style>
+
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'website915',
+});
+useHead({
+  title: 'About 915website | Web Development and Design Services',
+  meta: [
+    { name: 'description', content: 'Web Development and Design services for local business owners in El Paso Texas.' }
+  ],
+});
+</script>
