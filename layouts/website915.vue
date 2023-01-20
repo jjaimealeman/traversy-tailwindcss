71e6a3ba798efd526894cbb0aae97b67b0e7c467
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-19 20:42:41 -0700

    Website layout / remove bg image, add new logo and new navigation.

--- layouts/website915.vue
+++ layouts/website915.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="bg-[url('~/assets/elpaso.jpg')] bg-no-repeat bg-cover bg-fixed">
-    <div class="flex flex-row items-center justify-center">
-      <div class="w-1/2 flex justify-end">
-        <img src="~/assets/915website-logo.svg" width="150" alt="">
-      </div>
-      <nav class="bg-zinc-800 text-white w-1/2 p-4 text-2xl">
-        <NuxtLink class="px-4" to="/"> Home </NuxtLink>
-        <NuxtLink class="px-4" to="/About"> About </NuxtLink>
-        <NuxtLink class="px-4" to="/Services"> Services </NuxtLink>
+  <div id="default_template"
+    class="container mx-auto pb-12 max-w-7xl bg-white text-zinc-800 dark:bg-zinc-800 dark:text-white">
+    <div class="flex flex-row items-start justify-between">
+
+      <img class="-rotate-12 w-24 md:w-28 lg:w-32 ml-4 mt-4" src="~/assets/915website-logo.svg" alt="">
+
+      <nav class="bg-white w-full p-4 text-lg md:text-xl lg:text-2xl text-right">
+        <NuxtLink class="pr-4" to="/"> Home </NuxtLink>
+        <NuxtLink class="pr-4" to="/"> About </NuxtLink>
+        <NuxtLink class="pr-4" to="/"> Services </NuxtLink>
+        <NuxtLink class="pr-4" to="/"> Contact </NuxtLink>
       </nav>
     </div>
 
