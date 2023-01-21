f9fc71b8d4022303692fbf005dfd16316df422d5
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-21 00:00:13 -0700

    Add bigger tap target to navigation with active-link colors.

--- layouts/website915.vue
+++ layouts/website915.vue
@@ -6,11 +6,11 @@
       <img class="-rotate-12 w-24 md:w-28 lg:w-32 ml-4 mt-4" src="~/assets/915website-logo.svg" width="135" height="140"
         alt="">
 
-      <nav class="bg-white w-full p-4 text-lg md:text-xl lg:text-2xl text-right">
-        <NuxtLink class="pr-4" to="/"> Home </NuxtLink>
-        <NuxtLink class="pr-4" to="/About"> About </NuxtLink>
-        <NuxtLink class="pr-4" to="/"> Services </NuxtLink>
-        <NuxtLink class="pr-4" to="/"> Contact </NuxtLink>
+      <nav class="bg-white w-full pr-4 pt-4 text-lg md:text-xl lg:text-2xl text-right">
+        <NuxtLink class="p-4" to="/"> Home </NuxtLink>
+        <NuxtLink class="p-4" to="/About"> About </NuxtLink>
+        <NuxtLink class="p-4" to="/Services"> Services </NuxtLink>
+        <NuxtLink class="p-4" to="/Contact"> Contact </NuxtLink>
       </nav>
     </div>
 
@@ -18,3 +18,21 @@
   </div>
 </template>
 
+<style scoped>
+.router-link-active, 
+.router-link-exact-active
+{
+  @apply border-b-4 border-sky-400 text-sky-500
+}
+a
+{
+  @apply border-b-2 border-sky-200 
+}
+
+
+
+/* .router-link-exact-active */
+/* { */
+/*   @apply text-sky-400  */
+/* } */
+</style>
